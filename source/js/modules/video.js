eb484acbaa88eb0809aad31f21d82462790bbc1f
Дементьева Инна <acorp.murka@gmail.com>
2023-04-26 20:36:29 +0300

    Добавляет блок промо

---
+++ source/js/modules/video.js
@@ -0,0 +1,12 @@
+const video = document.querySelector('.promo__video');
+
+video.addEventListener('click', function () {
+  if (video.classList.contains('ready')) {
+    return;
+  }
+  video.classList.add('ready');
+
+  const src = video.dataset.src;
+
+  video.insertAdjacentHTML('afterbegin', '<iframe src="' + src + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
+});
