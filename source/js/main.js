0e83915083f07d1f24e5fe1a9403cb94c3804574
Дементьева Инна <acorp.murka@gmail.com>
2023-05-01 20:48:57 +0300

    Добавляет блок формы

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
+// import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import './modules/video';
 import {initTabs} from './modules/tabs/init-tabs';
@@ -20,7 +20,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
 
     initTabs();
     initSwiper();
