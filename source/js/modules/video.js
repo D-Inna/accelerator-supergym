6d56944040af585571120f1a29b08498284e0915
Дементьева Инна <acorp.murka@gmail.com>
2023-05-04 17:52:34 +0300

    Добавляет стили табам

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -8,5 +8,5 @@ video.addEventListener('click', function () {
 
   const src = video.dataset.src;
 
-  video.insertAdjacentHTML('afterbegin', '<iframe width="366" height="230" src="' + src + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
+  video.insertAdjacentHTML('afterbegin', '<iframe src="' + src + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
 });
